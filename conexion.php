286625b91b99b28c7788f69fe362989e794354dd
daviddiazm <davidcol123dm@gmail.com>
2024-03-25 01:22:28 -0500

    agregar la conexion con la nueva base de datos

---
+++ conexion.php
@@ -0,0 +1,7 @@
+<?php
+
+// $conexion = mysqli_connect("localhost","id21809618_david","@Proyecto1","id21809618_david_db");
+// $conexion = mysqli_connect("localhost","root","","policia");
+$conexion = mysqli_connect("localhost","root","","policia");
+
+?>
