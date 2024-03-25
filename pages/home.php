e0fe80d554cc5ffd528d1e12346f9d3f651f5fa2
daviddiazm <davidcol123dm@gmail.com>
2024-03-25 01:24:41 -0500

    home con imagen principal y video fase 1

--- pages/home.php
+++ pages/home.php
@@ -3,7 +3,62 @@
 <body>
   <?php include("../components/header.php") ?>
   <main class="home">
-    <section class="home__im"></section>
+    <section class="main__canva">
+      <div class="main__canva-cuadrilatero"></div>
+      <article class="main__canva-text">
+        <h3 class="main__canva-text--h3">No estás solo. Denuncia el bullying.¡Juntos lo superaremos!</h3>
+      </article>
+      <img src="../assets/imgs/policias-ok.png" alt="" class="main__canva-img">
+      <!-- <video src="../assets/videos/CGI - Animação Arquitetura - Casa Contemporânea (Blender).mp4" class="main__canva-img" controls></video> -->
+      <!-- <video src="../assets/videos/CGI - Animação Arquitetura - Casa Contemporânea (Blender).mp4" class="main__canva-img" autoplay controls></video> -->
+    </section>
+
+    <!-- <section class="featured-videos">
+      <div class="container">
+        <h2>Videos Destacados</h2>
+        <div class="video-list">
+          <article class="video-item">
+              <iframe width="996" height="560" src="https://www.youtube.com/embed/E235oRqubv4" title="¿Qué es el ciberacoso?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p>Descripción del primer video.</p>
+          </article>
+          <article class="video-item">
+            <iframe width="996" height="560" src="https://www.youtube.com/embed/i_92-NovRT0" title="Los peligros del internet para los niños" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p>Descripción del segundo video.</p>
+          </article>
+          <article class="video-item">
+              <iframe width="840" height="560" src="https://www.youtube.com/embed/QQKlaxPfRa8" title="Bullying escolar, terapia psicológica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p>Descripción del tercer video.</p>
+          </article>
+        </div>
+      </div>
+    </section> -->
+    <section class="featured-videos">
+      <div class="container">
+        <h2>Videos Destacados</h2>
+        <div class="video-list">
+          <article class="video-item">
+            <iframe width="996" height="560" src="https://www.youtube.com/embed/E235oRqubv4" title="¿Qué es el ciberacoso?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p>Descripción del primer video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+          </article>
+          <article class="video-item">
+            <iframe width="996" height="560" src="https://www.youtube.com/embed/i_92-NovRT0" title="Los peligros del internet para los niños" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p>Descripción del segundo video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+          </article>
+          <article class="video-item">
+            <iframe width="840" height="560" src="https://www.youtube.com/embed/QQKlaxPfRa8" title="Bullying escolar, terapia psicológica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p>Descripción del tercer video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+          </article>
+          <article class="video-item">
+            <iframe width="996" height="560" src="https://www.youtube.com/embed/hMnUpfDJV8Y" title="¿Qué es la Violencia Escolar?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p>Descripción del tercer video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+          </article>
+          <article class="video-item">
+            <iframe width="996" height="560" src="https://www.youtube.com/embed/jQuMnfLtPB0" title="¿Qué es la violencia psicológica?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <p>Descripción del tercer video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+          </article>
+        </div>
+      </div>
+    </section>
   </main>
   <?php include("../components/footer.php") ?>
 </body>
