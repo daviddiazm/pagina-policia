3358d01a979ef19505b6c6b7be6ab52aefdfea97
daviddiazm <davidcol123dm@gmail.com>
2024-03-25 01:23:53 -0500

    ajustar mostrar denuncias para la nueva base de datos

---
+++ pages/mostrarDenuncias.php
@@ -0,0 +1,39 @@
+<?php include("../components/head.php") ?>
+
+<body>
+  <?php include("../components/header.php") ?>
+
+  <?php
+  include("../conexion.php");
+  if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+  }
+  $sql = "SELECT * FROM denuncia";
+  $resultado = $conexion->query($sql);
+  $conexion->close();
+
+  if ($resultado->num_rows > 0) {
+    while ($fila = $resultado->fetch_assoc()) {
+  ?>
+      <div class="denuncia">
+        <h2 class="">Tipo de anonimato: <?php echo $fila['tipo_anonimato']; ?></h2>
+        <p>ID de la denuncia: <?php echo $fila['id']; ?></p>
+        <p>Nombre del denunciante: <?php echo $fila['nombre']; ?></p>
+        <p>Teléfono del denunciante: <?php echo $fila['telefono']; ?></p>
+        <p>Correo electrónico del denunciante: <?php echo $fila['correo']; ?></p>
+        <p>Tipo de documento del denunciante: <?php echo $fila['tipo_documento']; ?></p>
+        <p>Número de documento del denunciante: <?php echo $fila['numero_documento']; ?></p>
+        <p>Contenido de la denuncia: <?php echo "<br>" . $fila['contenido']; ?></p>
+      </div>
+      <hr>
+  <?php
+    }
+  } else {
+    echo "No se encontraron resultados";
+  }
+  ?>
+
+  <?php include("../components/footer.php") ?>
+</body>
+
+</html>
