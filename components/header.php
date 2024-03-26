4319b664bc5e14765eeefad55f978a8efa8f4c64
daviddiazm <davidcol123dm@gmail.com>
2024-03-26 12:09:02 -0500

    modifique el header

--- components/header.php
+++ components/header.php
@@ -1,16 +1,22 @@
 <header class="header">
   <nav class="header__nav">
     <ul class="header__nav-ul">
-      <li class="header__nav-item"><a href="" class="header__nav-link-logo-container"><img src="../assets/imgs/logo.png" alt="" class="header__nav-link-logo-img"></a></li>
-      <li class="header__nav-item"><a href="../pages/home.php" class="header__nav-link">Inicio</a></li>
+      <a href="" class="header__nav-link-logo-container"><img src="../assets/imgs/logo.png" alt="" class="header__nav-link-logo-img"></a>
+      <!-- <li class="header__nav-item"><a href="" class="header__nav-link-logo-container"><img src="../assets/imgs/logo.png" alt="" class="header__nav-link-logo-img"></a></li> -->
+      <!-- <li class="header__nav-item"><a href="../pages/home.php" class="header__nav-link">Inicio</a></li> -->
+      <a href="../pages/home.php" class="header__nav-item">Inicio</a>
+      <a href="../pages/denuncia.php" class="header__nav-item">Denunciar</a>
+      <a href="../pages/home.php" class="header__nav-item">Sobre nosotros</a>
+      <a href="../pages/mostrarDenuncias.php" class="header__nav-item">Ver denuncias</a>
+      <!-- 
       <li class="header__nav-item"><a href="../pages/denuncia.php" class="header__nav-link">Ingresa tu denuncia</a></li>
       <li class="header__nav-item"><a href="#" class="header__nav-link">Sobre nosotros</a></li>
       <li class="header__nav-item"><a href="#" class="header__nav-link">Contactanos</a></li>
-      <li class="header__nav-item"><a href="../pages/mostrarDenuncias.php" class="header__nav-link">Ver denuncias</a></li>
+      <li class="header__nav-item"><a href="../pages/mostrarDenuncias.php" class="header__nav-link">Ver denuncias</a></li> -->
     </ul>
   </nav>
   <section class="header__login-register-container">
-    <button class="header__login-register-btn header__nav-item"><a href="../pages/loginSingUp.php">Inicia sesion Registrate</a></button>
+    <button class="header__login-register-btn "><a href="../pages/loginSingUp.php">Inicia sesion -- Registrate</a></button>
     <!-- <button class="header__login-register-btn header__nav-item"><a href="../pages/loginSingUp.php">Inicia sesion</a></button> -->
     <!-- <button class="header__login-login-btn header__nav-item"><a href="../pages/loginSingUp.php">Inicia sesion</a></button> -->
   </section>
