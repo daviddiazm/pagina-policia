b6192bb204ba805f1d06375174064dd01fd31851
daviddiazm <davidcol123dm@gmail.com>
2024-03-26 12:09:24 -0500

    home con video y lineas de atencion

--- pages/home.php
+++ pages/home.php
@@ -6,7 +6,8 @@
     <section class="main__canva">
       <div class="main__canva-cuadrilatero"></div>
       <article class="main__canva-text">
-        <h3 class="main__canva-text--h3">No estás solo. Denuncia el bullying.¡Juntos lo superaremos!</h3>
+        <h3 class="main__canva-text--h3">No estás solo. Denuncia el bullying.</h3>
+        <h3 class="main__canva-text--h3">¡Juntos lo superaremos!</h3>
       </article>
       <img src="../assets/imgs/policias-ok.png" alt="" class="main__canva-img">
       <!-- <video src="../assets/videos/CGI - Animação Arquitetura - Casa Contemporânea (Blender).mp4" class="main__canva-img" controls></video> -->
@@ -32,33 +33,57 @@
         </div>
       </div>
     </section> -->
+    <section class="lineas-atencion">
+      <h2>Lineas de atencion</h2>
+      <div class="lineas-atencion__container">
+
+        <article class="lineas-atencion__item">
+          <img src="../assets/imgs/intimidacion.png" alt="">
+          <a target="_blank" href="https://rutadeatencion.sdmujer.gov.co/">Linea de atencion para violencia de genero</a>
+        </article>
+
+        <article class="lineas-atencion__item">
+          <img src="../assets/imgs/policia.png" alt="">
+          <a target="_blank" href="https://srvcnpc.policia.gov.co/PSC/">Linea de atencion de la policia</a>
+        </article>
+
+        <article class="lineas-atencion__item">
+          <img src="../assets/imgs/violencia.png" alt="">
+          <a target="_blank" href="https://www.icbf.gov.co/mis-manos-te-ensenan/conoce-la-ruta-de-atencion-de-acoso-escolar-o-bullying">Linea de atencion del ICBF para el acoso escolar</a>
+        </article>
+
+      </div>
+    </section>
     <section class="featured-videos">
       <div class="container">
-        <h2>Videos Destacados</h2>
+        <h2 class="featured-videos-title">Videos Destacados</h2>
         <div class="video-list">
           <article class="video-item">
             <iframe width="996" height="560" src="https://www.youtube.com/embed/E235oRqubv4" title="¿Qué es el ciberacoso?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <p>Descripción del primer video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+            <p>Hola! Antes de ver este video, quiero decirte que aprenderás sobre algo llamado 'ciberacoso'. Es cuando alguien es malo contigo en internet. Pero no te preocupes, después de ver este video, sabrás cómo pedir ayuda si alguna vez te sucede a ti. Recuerda que siempre está bien hablar con un adulto de confianza si te sientes mal por algo que ves en internet. ¡Vamos a ver juntos cómo podemos detener el ciberacoso y hacer de internet un lugar más seguro para todos</p>
           </article>
           <article class="video-item">
             <iframe width="996" height="560" src="https://www.youtube.com/embed/i_92-NovRT0" title="Los peligros del internet para los niños" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <p>Descripción del segundo video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+            <p>¡Hola! Antes de ver este video, quiero advertirte sobre los riesgos que existen en internet. A veces, podemos encontrarnos con cosas que pueden ser peligrosas o hacernos sentir incómodos. Pero no te preocupes, después de ver este video, sabrás cómo protegerte y qué hacer si te enfrentas a algo así. Recuerda que siempre está bien hablar con un adulto de confianza si algo te preocupa mientras estás en línea. ¡Vamos a aprender juntos cómo hacer que internet sea un lugar más seguro para todos!</p>
           </article>
           <article class="video-item">
             <iframe width="840" height="560" src="https://www.youtube.com/embed/QQKlaxPfRa8" title="Bullying escolar, terapia psicológica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <p>Descripción del tercer video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+            <p>¡Hola! Antes de ver este video, quiero hablarte sobre el bullying en la escuela. A veces, algunas personas pueden tratar a otras de una manera que no es buena, haciéndoles sentir mal. Pero no te preocupes, después de ver este video, sabrás cómo protegerte y qué hacer si te enfrentas a situaciones así. Recuerda que siempre está bien hablar con un adulto de confianza si algo te está molestando en la escuela. ¡Vamos a aprender juntos cómo hacer que la escuela sea un lugar más seguro y amigable para todos!</p>
           </article>
           <article class="video-item">
             <iframe width="996" height="560" src="https://www.youtube.com/embed/hMnUpfDJV8Y" title="¿Qué es la Violencia Escolar?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <p>Descripción del tercer video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+            <p>¡Hola! Antes de ver este video, quiero mencionarte algo importante sobre la violencia en la escuela. A veces, pueden suceder cosas donde alguien actúa de manera agresiva o violenta, y eso está mal. Pero no te preocupes, después de ver este video, sabrás cómo protegerte y qué hacer si te encuentras en una situación así. Recuerda que siempre está bien hablar con un adulto de confianza si algo te está molestando en la escuela. ¡Vamos a aprender juntos cómo hacer que la escuela sea un lugar más seguro y amable para todos!</p>
           </article>
           <article class="video-item">
             <iframe width="996" height="560" src="https://www.youtube.com/embed/jQuMnfLtPB0" title="¿Qué es la violencia psicológica?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <p>Descripción del tercer video. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim neque architecto quo reprehenderit id corporis similique. Veritatis, aut provident eum laboriosam maiores possimus quo inventore, deleniti fugit hic minus.</p>
+            <p>¡Hola! Antes de comenzar este video, quisiera hablar contigo sobre la violencia psicológica. A veces, pueden suceder cosas en las que alguien intenta hacerte daño usando palabras o comportamientos que te hacen sentir mal emocionalmente. Pero no te preocupes, después de ver este video, sabrás cómo cuidarte y qué hacer si te encuentras en una situación así. Recuerda que siempre es válido hablar con un adulto en quien confíes si algo te está molestando. ¡Vamos a aprender juntos cómo crear un ambiente más seguro y acogedor para todos!</p>
           </article>
         </div>
       </div>
     </section>
+
+
+    <section class="noticias"></section>
   </main>
   <?php include("../components/footer.php") ?>
 </body>
