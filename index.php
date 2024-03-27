f325f74da8cc2ddb1bcbdf7379908bd8610a4c53
daviddiazm <davidcol123dm@gmail.com>
2024-03-27 16:30:23 -0500

    se modifico el index para que si inicia session lo mande a mostrarDenuncias.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-header("Location: ./pages/home.php");
+session_start();
+if(isset($_SESSION['usuario'])) {
+  header("Location: ./pages/mostrarDenuncias.php");
+} else {
+  header("Location: ./pages/home.php");
+}
 
 
 ?>
